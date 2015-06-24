9f5f461d37534cec496f871deb7e6965f727022d
jeanmarcraymond <jeanmarc.raymond@yahoo.ca>
2015-06-24 13:57:47 -0400

    look and feel updates

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 app.use('/build',express.static('build'));
+app.use('/static',express.static('static'));
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
